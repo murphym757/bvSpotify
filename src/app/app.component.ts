0b63b5b3f09cdde91acf969583028712740f403b
Maurice Murphy <murphym757@yahoo.com>
2018-02-09 00:13:45 -0500

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   moduleId: module.id,
