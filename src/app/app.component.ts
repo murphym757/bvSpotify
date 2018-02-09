e7f0fb04e1854475869146bd37cff57c8d2c08af
Maurice Murphy <murphym757@yahoo.com>
2018-02-09 00:05:08 -0500

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
