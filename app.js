0f7dd3f2be3230b95bf142e263d087c25f41d5c7
Maurice Murphy <murphym757@yahoo.com>
2018-02-22 22:38:45 -0500

    corrected heroku eroor

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Install express server
 const express = require('express'),
       cors = require('cors'),
+      router = express.Router(),
       app = express();
 
 router.get('/', function (req, res) {
