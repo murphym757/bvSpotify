16bc6792ccc49d06072215b3040114f39ace95a8
Maurice Murphy <murphym757@yahoo.com>
2018-02-23 11:09:04 -0500

    testing CORS

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express'),
 
 // allow-cors
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://bvspotify.herokuapp.com/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })
