9651dbd5c59c8514df95522cf215b33fba04de7c
Maurice Murphy <murphym757@yahoo.com>
2018-02-23 11:54:44 -0500

    testing CORS

--- app.js
+++ app.js
@@ -4,18 +4,9 @@ const express = require('express'),
       router = express.Router(),
       app = express();
 
-app.use(function (req, res, next) {
-    var allowedOrigins = ['http://localhost:4200', 'http://localhost:8080', 'https://bvspotify.herokuapp.com'];
-    var origin = req.headers.origin;
-    if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    //res.header('Access-Control-Allow-Origin', 'https://bvspotify.herokuapp.com');
-    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    return next();
-});
+app.use(cors({
+    origin: 'https://bvspotify.herokuapp.com'
+}));
 
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
