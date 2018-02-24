06a19c5fb0b6dcc0097ba34eaa17df189079b532
Maurice Murphy <murphym757@yahoo.com>
2018-02-24 18:11:23 -0500

    testing CORS

--- app.js
+++ app.js
@@ -4,34 +4,13 @@ const cors = require('cors');
 const router = express.Router();
 const app = express();
 
-var app = http.createServer(function (req, res, next) {
-    var headers = {};
-
-    // set header to handle the CORS
-    headers['Access-Control-Allow-Origin'] = '*';
-    headers['Access-Control-Allow-Headers'] = 'Authorization';
-    headers['Access-Contrl-Allow-Methods'] = 'POST, OPTIONS';
-    headers["Access-Control-Max-Age"] = '86400';
-    res.writeHead(200, headers);
-
-    if (req.method === 'OPTIONS') {
-        console.log('OPTIONS SUCCESS');
-        res.end();
-    }
-    else {
-        //other requests
-    }
+app.use(function (req, res, next) {
+    res.setheader("Access-Control-Allow-Origin", "*");
+    res.setheader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setheader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next(console.log('Hello'));
 });
-
-// Check if preflight request
-if (req.method === 'OPTIONS') {
-    res.status(200);
-    res.end();
-}
-else {
-    // Pass to next layer of middleware
-    next();
-}
 
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/src'));
