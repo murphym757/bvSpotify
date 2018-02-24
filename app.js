26aec7fb849deb12c01976c66c27acc5dd40c1a0
Maurice Murphy <murphym757@yahoo.com>
2018-02-24 17:35:17 -0500

    testing CORS

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //Install express server
-const express = require('express');
-const cors = require('cors');
-const router = express.Router();
-const app = express();
+const express = require('express'),
+      app = express(),
+      cors = require('cors'),
+      path = require('path'),
+      router = express.Router(),
+      port = process.env.PORT || 3000;
+
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", 'http://bvspotify.herokuapp.com/'); //<-- you can change this with a specific url like http://localhost:4200
@@ -13,7 +16,15 @@ app.use(function (req, res, next) {
 });
 
 // Serve only the static files from the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/src/index.html'));
+});
+
+// process.env.PORT lets the port be set by Heroku
+// use port 3000 unless there exists a preconfigured port ("port" is called with the other dependencies)
+//Localhost server
+app.listen(port, () => {
+    console.log('Server started on port ' + port)
+});
