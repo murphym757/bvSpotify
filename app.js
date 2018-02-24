d0f38cee27a5395146d4b7416c9fbbfdc727d7dd
Maurice Murphy <murphym757@yahoo.com>
2018-02-24 18:14:56 -0500

    testing CORS

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const app = express();
 
 app.use(function (req, res, next) {
-    res.setheader("Access-Control-Allow-Origin", "*");
-    res.setheader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setheader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Credentials', true);
     next(console.log('Hello'));
 });
 
