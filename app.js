84cbc2f4b3f022a3f3c70eb3ca6864b6879b4a83
Maurice Murphy <murphym757@yahoo.com>
2018-02-24 18:07:22 -0500

    testing CORS

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
 //Install express server
-const express = require('express'),
-      app = express(),
-      cors = require('cors'),
-      path = require('path'),
-      router = express.Router(),
-      port = process.env.PORT || 3000;
+const express = require('express');
+const cors = require('cors');
+const router = express.Router();
+const app = express();
 
+var app = http.createServer(function (req, res, next) {
+    var headers = {};
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://bvspotify.herokuapp.com/'); //<-- you can change this with a specific url like http://localhost:4200
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
+    // set header to handle the CORS
+    headers['Access-Control-Allow-Origin'] = '*';
+    headers['Access-Control-Allow-Headers'] = 'Authorization';
+    headers['Access-Contrl-Allow-Methods'] = 'POST, OPTIONS';
+    headers["Access-Control-Max-Age"] = '86400';
+    res.writeHead(200, headers);
+
+    if (req.method === 'OPTIONS') {
+        console.log('OPTIONS SUCCESS');
+        res.end();
+    }
+    else {
+        //other requests
+    }
 });
+
+// Check if preflight request
+if (req.method === 'OPTIONS') {
+    res.status(200);
+    res.end();
+}
+else {
+    // Pass to next layer of middleware
+    next();
+}
 
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/src'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
-});
-
-// process.env.PORT lets the port be set by Heroku
-// use port 3000 unless there exists a preconfigured port ("port" is called with the other dependencies)
-//Localhost server
-app.listen(port, () => {
-    console.log('Server started on port ' + port)
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
