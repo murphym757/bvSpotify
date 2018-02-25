0d7aeb3f73987f4392596c26fef212159db7fd3b
Maurice Murphy <murphym757@yahoo.com>
2018-02-24 23:04:41 -0500

    testing CORS

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ const express = require('express'),
 
 app.use(cors());
 
-app.all("*", function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
-    return next();
-});
-
-app.use(allowCrossDomain);
 
 // Serve only the static files from the dist directory
 app.use(express.static(__dirname + '/dist'));
